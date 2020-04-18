7215ec8ee50477dcb1096063b75c3bd2481bdc50
Duke Yin <yduke@hotmail.com>
2020-04-18 23:02:13 +0800

    add continents data

--- public/js/corona-virus-data-public.js
+++ public/js/corona-virus-data-public.js
@@ -3,7 +3,9 @@
 	$(document).ready(function() {
 		let url = "https://corona.lmao.ninja/v2/all";
 		let urlt = "https://corona.lmao.ninja/v2/countries/" + cov__contry + "?strict=false";
+		let urlc = "https://corona.lmao.ninja/v2/continents";
 		function eNum(x){return x.toLocaleString('en')}
+//world
 		$.getJSON(url, function(data, status) {
 			$("#cov-total-cases").append(eNum(data["cases"]));
 			$("#cov-total-recoverd").append(eNum(data["recovered"]));
@@ -18,7 +20,7 @@
 		}).fail(function() {
 			$("#cov-time").html(cov__err_msg);
 		});
-
+//single country
 		$.getJSON(urlt, function(data, status) {
 			$("#cov_flag").append('<img class="cov_flag" src="' + cov__plugin_dir + 'public/img/flags/' + data.countryInfo.iso2.toLowerCase() + '.png"> ');
 			$("#cov-new-confirm").append(eNum(data.cases ? data.cases : 'n/A'));
@@ -33,7 +35,68 @@
 		}).fail(function() {
 			$("#cov-time").html(cov__err_msg);
 		});
-
+//continents
+		$.getJSON(urlc, function(data, status) {
+			//South America
+			$("#cov-na-cases").append(eNum(data[0]["cases"]));
+			$("#cov-na-recoverd").append(eNum(data[0]["recovered"]));
+			$("#cov-na-dead").append(eNum(data[0]["deaths"]));
+			$("#cov-na-active").append(eNum(data[0]["active"]));
+			$("#cov-na-today-case").append(eNum(data[0]["todayCases"]));
+			$("#cov-na-today-deaths").append(eNum(data[0]["todayDeaths"]));
+			$("#cov-na-critical").append(eNum(data[0]["critical"]));
+			$("#cov-time-na").text(timestampToTime(data[0].updated, 1));
+			//Europe
+			$("#cov-eu-cases").append(eNum(data[1]["cases"]));
+			$("#cov-eu-recoverd").append(eNum(data[1]["recovered"]));
+			$("#cov-eu-dead").append(eNum(data[1]["deaths"]));
+			$("#cov-eu-active").append(eNum(data[1]["active"]));
+			$("#cov-eu-today-case").append(eNum(data[1]["todayCases"]));
+			$("#cov-eu-today-deaths").append(eNum(data[1]["todayDeaths"]));
+			$("#cov-eu-critical").append(eNum(data[1]["critical"]));
+			$("#cov-time-eu").text(timestampToTime(data[1].updated, 1));
+			//Asia
+			$("#cov-as-cases").append(eNum(data[2]["cases"]));
+			$("#cov-as-recoverd").append(eNum(data[2]["recovered"]));
+			$("#cov-as-dead").append(eNum(data[2]["deaths"]));
+			$("#cov-as-active").append(eNum(data[2]["active"]));
+			$("#cov-as-today-case").append(eNum(data[2]["todayCases"]));
+			$("#cov-as-today-deaths").append(eNum(data[2]["todayDeaths"]));
+			$("#cov-as-critical").append(eNum(data[2]["critical"]));
+			$("#cov-time-as").text(timestampToTime(data[2].updated, 1));
+			//South America
+			$("#cov-sa-cases").append(eNum(data[3]["cases"]));
+			$("#cov-sa-recoverd").append(eNum(data[3]["recovered"]));
+			$("#cov-sa-dead").append(eNum(data[3]["deaths"]));
+			$("#cov-sa-active").append(eNum(data[3]["active"]));
+			$("#cov-sa-today-case").append(eNum(data[3]["todayCases"]));
+			$("#cov-sa-today-deaths").append(eNum(data[3]["todayDeaths"]));
+			$("#cov-sa-critical").append(eNum(data[3]["critical"]));
+			$("#cov-time-sa").text(timestampToTime(data[3].updated, 1));
+			//Oceania
+			$("#cov-oa-cases").append(eNum(data[4]["cases"]));
+			$("#cov-oa-recoverd").append(eNum(data[4]["recovered"]));
+			$("#cov-oa-dead").append(eNum(data[4]["deaths"]));
+			$("#cov-oa-active").append(eNum(data[4]["active"]));
+			$("#cov-oa-today-case").append(eNum(data[4]["todayCases"]));
+			$("#cov-oa-today-deaths").append(eNum(data[4]["todayDeaths"]));
+			$("#cov-oa-critical").append(eNum(data[4]["critical"]));
+			$("#cov-time-oa").text(timestampToTime(data[4].updated, 1));
+			//Africa
+			$("#cov-af-cases").append(eNum(data[5]["cases"]));
+			$("#cov-af-recoverd").append(eNum(data[5]["recovered"]));
+			$("#cov-af-dead").append(eNum(data[5]["deaths"]));
+			$("#cov-af-active").append(eNum(data[5]["active"]));
+			$("#cov-af-today-case").append(eNum(data[5]["todayCases"]));
+			$("#cov-af-today-deaths").append(eNum(data[5]["todayDeaths"]));
+			$("#cov-af-critical").append(eNum(data[5]["critical"]));
+			$("#cov-time-af").text(timestampToTime(data[5].updated, 1));			
+			$(".cov-loading1").fadeOut("slow");
+		}).fail(function() {
+			$("#cov-time").html(cov__err_msg);
+		});
+		
+//table all data
 //	function desc(x,y){
 //       return y.confirmed-x.confirmed
 //   }
